fee792994059d61fd2151365bb89353508607e83
EirikNilsson <eirik.nilsson@MacBook-Air-som-tillhor-EirikNilsson.local>
2026-01-21 10:56:20 +0100

    added images

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,25 +1,51 @@
 import { Tabs, Link } from "expo-router"
-import { Pressable, Text } from "react-native"
+import { Pressable, Text, View} from "react-native"
 import { useSelector } from "react-redux"
 import { RootState } from "../../store"
+import { Image } from 'expo-image'
+const logo = require("../../assets/images/logo.png")
+
+
+
 
 function HeaderRight() {
   const user = useSelector((state: RootState) => state.auth.currentUser)
 
   if (!user) {
     return (
-      <Link href="/login" asChild>
-        <Pressable style={{ marginRight: 16 }}>
-          <Text style={{ fontSize: 16 }}>Login</Text>
-        </Pressable>
-      </Link>
+        <View style={{ marginLeft: 16 , flexDirection: "row", alignItems: "center", gap: 8 }}>
+            <Image
+              source={logo}
+              style={{ width: 80, height: 80}}
+                contentFit="contain"    
+            />
+            <Link href="/login" asChild>
+                <Pressable style={{ marginRight: 16 }}>
+                <Text style={{ fontSize: 16 }}>Login</Text>
+                </Pressable>
+            </Link>
+        </View>
     )
   }
 
   return (
-    <Text style={{ marginRight: 16 }}>
-      {user.name}
-    </Text>
+    <View
+      style={{
+        marginRight: 16,
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 8,
+      }}
+    >
+        <Image
+            source={logo}
+            style={{ width: 80, height: 80}}
+            contentFit="contain"    
+        />
+      <Text style={{ fontSize: 16 }}>{user.name}</Text>
+  
+    </View>
+
   )
 }
 
@@ -43,5 +69,6 @@ export default function Layout() {
         options={{ title: "Profile" }}
       />
     </Tabs>
+    
   )
 }
