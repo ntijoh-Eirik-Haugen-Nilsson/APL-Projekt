09694b567d1d29adae8fd974271676c80378e511
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2026-01-22 10:01:48 +0100

    slice can now add bookings without any textfeilds or visuals

--- frontend/app/(tabs)/booking.tsx
+++ frontend/app/(tabs)/booking.tsx
@@ -1,25 +1,50 @@
-import { Text, View } from "react-native";
 import { Image } from "expo-image";
-const box = require("../../assets/images/box.png")
+import { Pressable, Text, View } from "react-native";
+import { useDispatch } from "react-redux";
+import { book } from "../../store/BookingSlice";
+
+const box = require("../../assets/images/box.png");
 
 export default function Booking() {
+  const dispatch = useDispatch();
+
+  const handleBooking = () => {
+    dispatch(
+      book({
+        name: "booking",
+        date_booked: new Date().toISOString(),
+      }),
+    );
+  };
+
   return (
     <View
-    style={{
-      backgroundColor: "red",
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
+      style={{
+        backgroundColor: "red",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
       <Image
         source={box}
         style={{ width: 200, height: 200 }}
         contentFit="contain"
-      >
-
-      </Image>
+      ></Image>
       <Text>Your package</Text>
+
+      <Pressable
+        onPress={handleBooking}
+        style={{
+          padding: 10,
+          backgroundColor: "white",
+          borderRadius: 5,
+          alignItems: "center",
+          marginTop: 10,
+        }}
+      >
+        <Text style={{ fontSize: 16 }}>Book</Text>
+      </Pressable>
     </View>
   );
 }
---
+++ frontend/store/BookingSlice.tsx
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type Booking = {
+  name: string;
+  date_booked: string;
+};
+
+type BookingState = {
+  bookings: Booking[];
+};
+
+const initialState: BookingState = {
+  bookings: [],
+};
+
+const bookingSlice = createSlice({
+  name: "bookings",
+  initialState,
+  reducers: {
+    book(state, action: PayloadAction<Booking>) {
+      state.bookings.push(action.payload);
+      console.log(state.bookings);
+    },
+  },
+});
+
+export const { book } = bookingSlice.actions;
+export default bookingSlice.reducer;
